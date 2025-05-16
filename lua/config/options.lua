88475232b1502643f3dec144d57366390c42a5da
Rui Sun <sunrui_2015@outlook.com>
2025-05-16 21:06:40 +0800

    Add syntax for visual studio xml files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Add support for C# and XAML files
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.xaml" }, command = "setf xml" })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.csproj" }, command = "setf xml" })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.sln" }, command = "setf xml" })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.axaml" }, command = "setf xml" })
