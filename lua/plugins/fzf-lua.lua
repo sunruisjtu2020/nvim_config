d3a72d7ae33228253e6d860518e59c193210d770
Rui Sun <sunrui_2015@outlook.com>
2025-06-06 09:37:07 +0800

    Add keybind for live grep

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,11 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  -- dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  keys = {
+    { "<leader>fl", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
+  },
+}
