f81bd07f0a7269fa919a5097c94e369fe3035ce5
Rui Sun <sunrui_2015@outlook.com>
2025-06-17 18:29:03 +0800

    Change cursor animation

---
+++ lua/plugins/animate.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.animate",
+  enabled = true,
+  opts = {
+    -- Cursor path
+    cursor = {
+      -- Whether to enable this animation
+      enable = false,
+    },
+  },
+}
